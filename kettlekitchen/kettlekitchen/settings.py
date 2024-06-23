cdb6acc1c6b96373c48b17d5e4a23336e0eff0a9
Kaushik Joshi <76639214+k-k-j123@users.noreply.github.com>
2024-06-23 23:06:39 +0530

    Update settings.py

--- kettlekitchen/kettlekitchen/settings.py
+++ kettlekitchen/kettlekitchen/settings.py
@@ -75,10 +75,11 @@ WSGI_APPLICATION = 'kettlekitchen.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.getenv('DB_NAME', 'path/to/your/db.sqlite3'),
+        'NAME': os.getenv('DB_NAME', os.path.join(BASE_DIR, 'db.sqlite3')),
     }
 }
 
